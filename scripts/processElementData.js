9d5ed3388bdb656eab58d087b758543379229156
AdrieleMaria <adrieleifrn@gmail.com>
2018-06-27 15:59:22 -0300

    EPF PROCESSOS

---
+++ scripts/processElementData.js
@@ -0,0 +1,75 @@
+//------------------------------------------------------------------------------
+// Copyright (c) 2005, 2006 IBM Corporation and others.
+// All rights reserved. This program and the accompanying materials
+// are made available under the terms of the Eclipse Public License v1.0
+// which accompanies this distribution, and is available at
+// http://www.eclipse.org/legal/epl-v10.html
+// 
+// Contributors:
+// IBM Corporation - initial implementation
+//------------------------------------------------------------------------------
+
+// This file will include dynamically generated process layout related data from 
+// the publishing service 
+contentPage.processPage.imageFiles["_re8swXVwEeirD9rQJF1XmQ,_ylSdUXVwEeirD9rQJF1XmQ,_dfp1cXVyEeirD9rQJF1XmQActivity"]="./../../FindiesProcess/deliveryprocesses/resources/Euristicas_A4C9185E_cc05f964_Activity.jpeg"
+contentPage.processPage.imageFiles["_re8swXVwEeirD9rQJF1XmQ,_1JWVgXVwEeirD9rQJF1XmQActivity"]="./../../FindiesProcess/deliveryprocesses/resources/MEP_F0CCF62D_6ec597f4_Activity.jpeg"
+contentPage.processPage.imageFiles["_re8swXVwEeirD9rQJF1XmQ,_1JWVgXVwEeirD9rQJF1XmQ,_m9Y7MXVyEeirD9rQJF1XmQActivity"]="./../../FindiesProcess/deliveryprocesses/resources/Implementação_4684199B_c202a3e3_Activity.jpeg"
+contentPage.processPage.imageFiles["_re8swXVwEeirD9rQJF1XmQ,_u6JoUXVwEeirD9rQJF1XmQActivity"]="./../../FindiesProcess/deliveryprocesses/resources/Design thinking_30891D2D_ae81bef4_Activity.jpeg"
+contentPage.processPage.imageFiles["_re8swXVwEeirD9rQJF1XmQ,_u6JoUXVwEeirD9rQJF1XmQ,_RJJBMnVxEeirD9rQJF1XmQActivity"]="./../../FindiesProcess/deliveryprocesses/resources/New Activity_C52605F6_681bb73e_Activity.jpeg"
+contentPage.processPage.imageFiles["_re8swXVwEeirD9rQJF1XmQActivity"]="./../../FindiesProcess/deliveryprocesses/resources/Findies process_F5B57C73_f5b57c73_Activity.jpeg"
+contentPage.processPage.imageFiles["_re8swXVwEeirD9rQJF1XmQ,_1JWVgXVwEeirD9rQJF1XmQ,_f6BmIXVyEeirD9rQJF1XmQActivity"]="./../../FindiesProcess/deliveryprocesses/resources/Levantamento de requisitos_F32FF270_6eae7cb8_Activity.jpeg"
+contentPage.processPage.imageFiles["_re8swXVwEeirD9rQJF1XmQ,_1JWVgXVwEeirD9rQJF1XmQ,_uuya8XVyEeirD9rQJF1XmQActivity"]="./../../FindiesProcess/deliveryprocesses/resources/Comunicação com o usuário_C2C1CE_7c414c16_Activity.jpeg"
+contentPage.processPage.imageFiles["_re8swXVwEeirD9rQJF1XmQ,_ylSdUXVwEeirD9rQJF1XmQActivity"]="./../../FindiesProcess/deliveryprocesses/resources/Euristica de Nielsen_414226EB_bf3ac8b2_Activity.jpeg"
+contentPage.processPage.imageFiles["_re8swXVwEeirD9rQJF1XmQ,_1JWVgXVwEeirD9rQJF1XmQ,_tZQLYXVyEeirD9rQJF1XmQActivity"]="./../../FindiesProcess/deliveryprocesses/resources/Implantação_6CA5C4E6_e8244f2e_Activity.jpeg"
+contentPage.processPage.imageFiles["_re8swXVwEeirD9rQJF1XmQ,_u6JoUXVwEeirD9rQJF1XmQ,_--vtUXVwEeirD9rQJF1XmQActivity"]="./../../FindiesProcess/deliveryprocesses/resources/Imersão_BDB0C005_60a6714d_Activity.jpeg"
+contentPage.processPage.imageFiles["_re8swXVwEeirD9rQJF1XmQ,_u6JoUXVwEeirD9rQJF1XmQ,_5FZe4XVxEeirD9rQJF1XmQActivity"]="./../../FindiesProcess/deliveryprocesses/resources/Prototipação_CD2CA625_7022576d_Activity.jpeg"
+contentPage.processPage.elementUrls["_--vtUXVwEeirD9rQJF1XmQ"] = ["Imersão","FindiesProcess/deliveryprocesses/Imersão_BDB0C005.html"];
+contentPage.processPage.elementUrls["_UXYQIHmVEeiE0d2eKw9b9g"] = ["Idealizacao do projeto ","FindiesProcess/deliveryprocesses/idealizacao_do_projeto__C5AF2948.html"];
+contentPage.processPage.elementUrls["_5FZe4XVxEeirD9rQJF1XmQ"] = ["Prototipação","FindiesProcess/deliveryprocesses/Prototipação_CD2CA625.html"];
+contentPage.processPage.elementUrls["_u6JoUXVwEeirD9rQJF1XmQ"] = ["Design thinking","FindiesProcess/deliveryprocesses/Design thinking_30891D2D.html"];
+contentPage.processPage.elementUrls["_DNngkXmaEeiE0d2eKw9b9g"] = ["equipe","FindiesProcess/deliveryprocesses/equipe_82AA309.html"];
+contentPage.processPage.elementUrls["_m9Y7MXVyEeirD9rQJF1XmQ"] = ["Implementação","FindiesProcess/deliveryprocesses/Implementação_4684199B.html"];
+contentPage.processPage.elementUrls["_M_yiEXmWEeiE0d2eKw9b9g"] = ["front-end","FindiesProcess/deliveryprocesses/front-end_7815C2A4.html"];
+contentPage.processPage.elementUrls["_yS8sAHmVEeiE0d2eKw9b9g"] = ["Consistencia de padroes","FindiesProcess/deliveryprocesses/consistencia_de_padroes_C1B5E48A.html"];
+contentPage.processPage.elementUrls["_0WhGQHmVEeiE0d2eKw9b9g"] = ["Liberdade do usuario","FindiesProcess/deliveryprocesses/liberdade_do_usuario_F62509CB.html"];
+contentPage.processPage.elementUrls["_HKil8HmWEeiE0d2eKw9b9g"] = ["Elaboracao de diagramas","FindiesProcess/deliveryprocesses/elaboracao_de_diagramas_C4615325.html"];
+contentPage.processPage.elementUrls["_RJJBMnVxEeirD9rQJF1XmQ"] = ["Ideação","FindiesProcess/deliveryprocesses/New Activity_C52605F6.html"];
+contentPage.processPage.elementUrls["_M_yiEHmWEeiE0d2eKw9b9g"] = ["Implementar a interface","FindiesProcess/deliveryprocesses/implementar_a_interface_6D77E294.html"];
+contentPage.processPage.elementUrls["_DNxRlXmaEeiE0d2eKw9b9g"] = ["front-end","FindiesProcess/deliveryprocesses/front-end_FA14E009.html"];
+contentPage.processPage.elementUrls["_wKlRUHmVEeiE0d2eKw9b9g"] = ["Prevencao de erros","FindiesProcess/deliveryprocesses/prevencao_de_erros_35315F1B.html"];
+contentPage.processPage.elementUrls["_sjBUwHmVEeiE0d2eKw9b9g"] = ["Flexibilidade e eficiencia no uso","FindiesProcess/deliveryprocesses/flexibilidade_e_eficiencia_no_uso_25723109.html"];
+contentPage.processPage.elementUrls["_GxGLMXmWEeiE0d2eKw9b9g"] = ["equipe","FindiesProcess/deliveryprocesses/equipe_26897C10.html"];
+contentPage.processPage.elementUrls["_ylSdUXVwEeirD9rQJF1XmQ"] = ["Euristica de Nielsen","FindiesProcess/deliveryprocesses/Euristica de Nielsen_414226EB.html"];
+contentPage.processPage.elementUrls["_2Lg5AHmVEeiE0d2eKw9b9g"] = ["correspondencia do sistema com o mundo real","FindiesProcess/deliveryprocesses/correspondencia_do_sistema_com_o_mundo_real_D1E31A9D.html"];
+contentPage.processPage.elementUrls["_neVS4HmVEeiE0d2eKw9b9g"] = ["Reconhecimento, diagnostico e recuperacao de erros","FindiesProcess/deliveryprocesses/reconhecimento_diagnostico_e_recuperacao_de_erros_50FA8E36.html"];
+contentPage.processPage.elementUrls["_l_Z2wXmVEeiE0d2eKw9b9g"] = ["equipe","FindiesProcess/deliveryprocesses/equipe_90B625BA.html"];
+contentPage.processPage.elementUrls["_f6BmIXVyEeirD9rQJF1XmQ"] = ["Levantamento de requisitos","FindiesProcess/deliveryprocesses/Levantamento de requisitos_F32FF270.html"];
+contentPage.processPage.elementUrls["_Q9j_M3mWEeiE0d2eKw9b9g"] = ["front-end","FindiesProcess/deliveryprocesses/front-end_9456D5B2.html"];
+contentPage.processPage.elementUrls["_DNxRkXmaEeiE0d2eKw9b9g"] = ["equipe","FindiesProcess/deliveryprocesses/equipe_3582FDEA.html"];
+contentPage.processPage.elementUrls["_re8swXVwEeirD9rQJF1XmQ"] = ["Findies process","FindiesProcess/deliveryprocesses/Findies process_F5B57C73.html"];
+contentPage.processPage.elementUrls["_tZQLYXVyEeirD9rQJF1XmQ"] = ["Implantação","FindiesProcess/deliveryprocesses/Implantação_6CA5C4E6.html"];
+contentPage.processPage.elementUrls["_l_Z2w3mVEeiE0d2eKw9b9g"] = ["usuario","FindiesProcess/deliveryprocesses/usuario_E8290F95.html"];
+contentPage.processPage.elementUrls["_NnCvUHmWEeiE0d2eKw9b9g"] = ["Implementar as funcionalidades da interface","FindiesProcess/deliveryprocesses/implementar_as_funcionalidades_da_interface_8F95E075.html"];
+contentPage.processPage.elementUrls["_GIyDcHmVEeiE0d2eKw9b9g"] = ["Entender os usuarios ","FindiesProcess/deliveryprocesses/entender_os_usuarios__26CD5E40.html"];
+contentPage.processPage.elementUrls["_Q9j_NXmWEeiE0d2eKw9b9g"] = ["usuario","FindiesProcess/deliveryprocesses/usuario_175CDF6.html"];
+contentPage.processPage.elementUrls["_Q9j_MHmWEeiE0d2eKw9b9g"] = ["Ajustes","FindiesProcess/deliveryprocesses/ajustes_32460BC7.html"];
+contentPage.processPage.elementUrls["_TaxBcHmWEeiE0d2eKw9b9g"] = ["Entender o usuario","FindiesProcess/deliveryprocesses/entender_o_usuario_D3ACCB.html"];
+contentPage.processPage.elementUrls["_TaxBcXmWEeiE0d2eKw9b9g"] = ["equipe","FindiesProcess/deliveryprocesses/equipe_B718CDB.html"];
+contentPage.processPage.elementUrls["_l_Z2wHmVEeiE0d2eKw9b9g"] = ["Ajuda e documentacao","FindiesProcess/deliveryprocesses/ajuda_e_documentacao_861845AA.html"];
+contentPage.processPage.elementUrls["_3WrroHmVEeiE0d2eKw9b9g"] = ["Visibilidadade de estado atual do sistema","FindiesProcess/deliveryprocesses/visibilidadade_de_estado_atual_do_sistema_2ABF846B.html"];
+contentPage.processPage.elementUrls["_H2o28HmWEeiE0d2eKw9b9g"] = ["Estabelecimento de prazos","FindiesProcess/deliveryprocesses/estabelecimento_de_prazos_5EA23A0C.html"];
+contentPage.processPage.elementUrls["_DNnglHmaEeiE0d2eKw9b9g"] = ["usuario","FindiesProcess/deliveryprocesses/usuario_C21EA518.html"];
+contentPage.processPage.elementUrls["_YRul0HmVEeiE0d2eKw9b9g"] = ["Prototipacao","FindiesProcess/deliveryprocesses/prototipacao_689CFF76.html"];
+contentPage.processPage.elementUrls["_1JWVgXVwEeirD9rQJF1XmQ"] = ["MEP","FindiesProcess/deliveryprocesses/MEP_F0CCF62D.html"];
+contentPage.processPage.elementUrls["_TaxBc3mWEeiE0d2eKw9b9g"] = ["usuario","FindiesProcess/deliveryprocesses/usuario_62E476B6.html"];
+contentPage.processPage.elementUrls["_Q9j_MXmWEeiE0d2eKw9b9g"] = ["back-end","FindiesProcess/deliveryprocesses/back-end_3CE3EBD7.html"];
+contentPage.processPage.elementUrls["_neVS43mVEeiE0d2eKw9b9g"] = ["front-end","FindiesProcess/deliveryprocesses/front-end_B30B5821.html"];
+contentPage.processPage.elementUrls["_GrDDAHmVEeiE0d2eKw9b9g"] = ["Entendimento do problema","FindiesProcess/deliveryprocesses/entendimento_do_problema_1B6B4620.html"];
+contentPage.processPage.elementUrls["_dfp1cXVyEeirD9rQJF1XmQ"] = ["Euristicas","FindiesProcess/deliveryprocesses/Euristicas_A4C9185E.html"];
+contentPage.processPage.elementUrls["_uuya8XVyEeirD9rQJF1XmQ"] = ["Comunicação com o usuário","FindiesProcess/deliveryprocesses/Comunicação com o usuário_C2C1CE.html"];
+contentPage.processPage.elementUrls["_GxGLMHmWEeiE0d2eKw9b9g"] = ["Analise do sistema","FindiesProcess/deliveryprocesses/analise_do_sistema_1BEB9C00.html"];
+contentPage.processPage.elementUrls["_HNoLoHmVEeiE0d2eKw9b9g"] = ["Questionar","FindiesProcess/deliveryprocesses/questionar_84272C2A.html"];
+contentPage.processPage.elementUrls["_NnMgUHmWEeiE0d2eKw9b9g"] = ["back-end","FindiesProcess/deliveryprocesses/back-end_8EEC85DC.html"];
+contentPage.processPage.elementUrls["_qOAlMHmVEeiE0d2eKw9b9g"] = ["Estetica e design minimalista","FindiesProcess/deliveryprocesses/estetica_e_design_minimalista_1702B012.html"];
+contentPage.processPage.elementUrls["_neVS4XmVEeiE0d2eKw9b9g"] = ["back-end","FindiesProcess/deliveryprocesses/back-end_5B986E46.html"];
+contentPage.processPage.elementUrls["_uN3F8HmVEeiE0d2eKw9b9g"] = ["Reconhecimento","FindiesProcess/deliveryprocesses/reconhecimento_D264DCAA.html"];
